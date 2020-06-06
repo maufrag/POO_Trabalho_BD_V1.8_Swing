4be9d4606d5723acb4d2617e30cc84f737348474
Mateus Torres Ferreira <34038838+maufrag@users.noreply.github.com>
2020-06-06 02:38:39 -0300

    update
    -Adição dos testes com java swing

--- src/main/ClasseMain.java
+++ src/main/ClasseMain.java
@@ -1,5 +1,15 @@
 package main;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import CRUD.*;
@@ -10,6 +20,28 @@ public class ClasseMain {
 
 	public static void main(String[] args) {
 		System.out.println("Programa iniciado\n");
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();//é preciso usar um painel para agrupar esses componente
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		
+		List<String> listaMenu = obterItensMenu();
+		for(String menu : listaMenu) {
+			Button botao = new Button(menu);
+			botao.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					irParaOpcaoSelecionada(menu);
+					}
+			});
+			//botao.setBounds(10, 10, 10, 10);
+			//botao.setPreferredSize(new Dimension(400, 400));
+			panel.add(botao);
+		}
+		
+		frame.add(panel);//coloca o conteudo do painel no frame
+		//frame.setSize(500, 500);//define o tamanho da janela
+		frame.pack();
+		frame.setVisible(true);//coloca a janela visivel
 		montarMenu(obterItensMenu());
 		System.out.println("\nPrograma encerrado");
 	}
@@ -19,7 +51,7 @@ public class ClasseMain {
 		do {
 			MetodosDeApoio.listarMenu(listaMenu);
 			valor = MetodosDeApoio.obterInputTratado(1, listaMenu.size());
-			irParaOpcaoSelecionada(valor);
+			//irParaOpcaoSelecionada(valor);
 
 		} while (valor != listaMenu.size());
 
@@ -29,19 +61,22 @@ public class ClasseMain {
 		return Arrays.asList("Create", "Read", "Update", "Delete", "Sair");
 	}
 
-	public static void irParaOpcaoSelecionada(int numeroOpcao) {
-		switch (numeroOpcao) {
-		case 1:
+	public static void irParaOpcaoSelecionada(String nomeMenu) {
+		switch (nomeMenu) {
+		case "Create":
 			Create.menuCreate();
 			break;
-		case 2:
+		case "Read":
 			Read.readMenu();
 			break;
-		case 3:
+		case "Update":;
 			Update.updateMenu();
 			break;
-		case 4:
+		case "Delete":
 			Delete.deleteMenu();
+			break;
+		case "Sair":
+			//TODO
 			break;
 		}
 	}
