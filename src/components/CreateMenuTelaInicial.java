803a44e36fa700a1de7bd3d121c4dcbb0f2e9efc
Mateus Torres Ferreira <34038838+maufrag@users.noreply.github.com>
2020-06-08 13:35:33 -0300

    ajustes
    ajustes na tela inicial do create menu

--- src/components/CreateMenuTelaInicial.java
+++ src/components/CreateMenuTelaInicial.java
@@ -15,6 +15,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.SwingConstants;
 
 public class CreateMenuTelaInicial extends JPanel {
 
@@ -23,58 +24,46 @@ public class CreateMenuTelaInicial extends JPanel {
 	 */
 	public CreateMenuTelaInicial(JPanel panelAtual, JPanel panelPrincipal, JFrame frame) {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 93, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-				0.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-				Double.MIN_VALUE };
+		gridBagLayout.columnWidths = new int[] { 48, 0, 0, 0, 13, 63, 0 };
+		gridBagLayout.rowHeights = new int[] { 14, 0, 50, 20, 20, 20, 94, 0, 0, 30, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		gridBagLayout.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 		setLayout(gridBagLayout);
 
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 5, 5);
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		add(panel, gbc_panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 0 };
-		gbl_panel.rowHeights = new int[] { 0 };
-		gbl_panel.columnWeights = new double[] { Double.MIN_VALUE };
-		gbl_panel.rowWeights = new double[] { Double.MIN_VALUE };
-		panel.setLayout(gbl_panel);
-
 		JLabel lblNewLabel = new JLabel("Bem vindo ao create menu.");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 7;
-		gbc_lblNewLabel.gridy = 6;
+		gbc_lblNewLabel.gridx = 3;
+		gbc_lblNewLabel.gridy = 4;
 		add(lblNewLabel, gbc_lblNewLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("Gosstaria de cadastrar um produto?");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 7;
-		gbc_lblNewLabel_1.gridy = 7;
+		gbc_lblNewLabel_1.gridx = 3;
+		gbc_lblNewLabel_1.gridy = 5;
 		add(lblNewLabel_1, gbc_lblNewLabel_1);
 
 		JButton btnNewButton = new JButton("Prosseguir");
+		btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 5;
-		gbc_btnNewButton.gridy = 11;
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 8;
 		add(btnNewButton, gbc_btnNewButton);
-
+		procederCadastro(btnNewButton, panelAtual, panelPrincipal, frame);
 		JButton btnNewButton_1 = new JButton("Voltar");
+		btnNewButton_1.setVerticalAlignment(SwingConstants.BOTTOM);
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.anchor = GridBagConstraints.NORTH;
+		gbc_btnNewButton_1.anchor = GridBagConstraints.NORTHWEST;
 		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_1.gridx = 9;
-		gbc_btnNewButton_1.gridy = 11;
+		gbc_btnNewButton_1.gridx = 5;
+		gbc_btnNewButton_1.gridy = 8;
 		add(btnNewButton_1, gbc_btnNewButton_1);
 		MetodosDeApoioView.voltar(btnNewButton_1, panelAtual, panelPrincipal, frame);
-		procederCadastro(btnNewButton, panelAtual, panel, frame);
 	}
 
 	private void procederCadastro(JButton button, JPanel novoPanel, JPanel panelPrincipal, JFrame frame) {
