8d966704ea13cdf41622df1720d6023609f26aa5
Mateus Torres Ferreira <34038838+maufrag@users.noreply.github.com>
2020-06-09 01:06:05 -0300

    Ajuste

--- src/components/CreateMenuTelaInicial.java
+++ src/components/CreateMenuTelaInicial.java
@@ -29,43 +29,43 @@ public class CreateMenuTelaInicial extends JPanel {
 	 */
 	public CreateMenuTelaInicial(JPanel panelAtual, JPanel panelPrincipal, JFrame frame) {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 48, 0, 0, 0, 13, 63, 0 };
+		gridBagLayout.columnWidths = new int[] { 0, 40, 0, -11, 0, 0, 0, 0, 13, 63, -16 };
 		gridBagLayout.rowHeights = new int[] { 14, 0, 50, 20, 20, 20, 94, 0, 0, 30, 0 };
-		gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 		gridBagLayout.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 		setLayout(gridBagLayout);
-
-		JLabel lblNewLabel = new JLabel("Bem vindo ao create menu.");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 3;
-		gbc_lblNewLabel.gridy = 4;
-		add(lblNewLabel, gbc_lblNewLabel);
-
-		JLabel lblNewLabel_1 = new JLabel("Gosstaria de cadastrar um produto?");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 3;
-		gbc_lblNewLabel_1.gridy = 5;
-		add(lblNewLabel_1, gbc_lblNewLabel_1);
-
-		JButton btnNewButton = new JButton("Prosseguir");
-		btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 8;
-		add(btnNewButton, gbc_btnNewButton);
-		procederCadastro(btnNewButton, panelAtual, panelPrincipal, frame);
+						
+								JLabel lblNewLabel = new JLabel("Bem vindo ao create menu.");
+								lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+								GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+								gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+								gbc_lblNewLabel.gridx = 6;
+								gbc_lblNewLabel.gridy = 4;
+								add(lblNewLabel, gbc_lblNewLabel);
+				
+						JLabel lblNewLabel_1 = new JLabel("Gostaria de cadastrar um produto?");
+						lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+						GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+						gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+						gbc_lblNewLabel_1.gridx = 6;
+						gbc_lblNewLabel_1.gridy = 5;
+						add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+				JButton btnNewButton = new JButton("Prosseguir");
+				btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
+				GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+				gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
+				gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+				gbc_btnNewButton.gridx = 5;
+				gbc_btnNewButton.gridy = 8;
+				add(btnNewButton, gbc_btnNewButton);
+				procederCadastro(btnNewButton, panelAtual, panelPrincipal, frame);
 		JButton btnNewButton_1 = new JButton("Voltar");
 		btnNewButton_1.setVerticalAlignment(SwingConstants.BOTTOM);
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
 		gbc_btnNewButton_1.anchor = GridBagConstraints.NORTHWEST;
 		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_1.gridx = 5;
+		gbc_btnNewButton_1.gridx = 8;
 		gbc_btnNewButton_1.gridy = 8;
 		add(btnNewButton_1, gbc_btnNewButton_1);
 		MetodosDeApoioView.voltar(btnNewButton_1, panelAtual, panelPrincipal, frame);
